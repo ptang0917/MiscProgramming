4d227ddad8e5511479ea561db93f58a01c8927fe
ptang0917 <ptang24@gmail.com>
2017-01-13 02:40:09 -0500

    Update BlackJack.cpp

--- Cplusplus/BlackJack.cpp
+++ Cplusplus/BlackJack.cpp
@@ -129,37 +129,37 @@ int calculateScore(string s) {
 	int score = 0;
 	int acevalue = 0;
 
-		if (s.at(0) == '2') {
-			score += 2;
-		}
-		else if (s.at(0) == '3') {
-			score += 3;
-		}
-		else if (s.at(0) == '4') {
-			score += 4;
-		}
-		else if (s.at(0) == '5') {
-			score += 5;
-		}
-		else if (s.at(0) == '6') {
-			score += 6;
-		}
-		else if (s.at(0) == '7') {
-			score += 7;
-		}
-		else if (s.at(0) == '8') {
-			score += 8;
-		}
-		else if (s.at(0) == '9') {
-			score += 9;
-		}
-		else if (s.at(0) == 'J' || s.at(0) == 'Q' || s.at(0) == 'K') {
-			score += 10;
-		}
-		else if (s.at(0) == '1' && s.at(1) == '0') {
-			score += 10;
-		}
-	
+	if (s.at(0) == '2') {
+		score += 2;
+	}
+	else if (s.at(0) == '3') {
+		score += 3;
+	}
+	else if (s.at(0) == '4') {
+		score += 4;
+	}
+	else if (s.at(0) == '5') {
+		score += 5;
+	}
+	else if (s.at(0) == '6') {
+		score += 6;
+	}
+	else if (s.at(0) == '7') {
+		score += 7;
+	}
+	else if (s.at(0) == '8') {
+		score += 8;
+	}
+	else if (s.at(0) == '9') {
+		score += 9;
+	}
+	else if (s.at(0) == 'J' || s.at(0) == 'Q' || s.at(0) == 'K') {
+		score += 10;
+	}
+	else if (s.at(0) == '1' && s.at(1) == '0') {
+		score += 10;
+	}
+
 	return score;
 }
 
@@ -203,12 +203,14 @@ int main() {
 	string playerCard;
 	string singleCard;
 
+	char answer;
+
 	//bool variables
 	bool inGame = true;
 	bool enableCheck;
 	bool playerTurnend;
 	bool playerLost;
-
+	bool dealerHit;
 
 	//Debugging Section
 
@@ -228,254 +230,314 @@ int main() {
 	// cout << "Printing Deck After Shuffle for the Second Time" << endl;
 	// deck.printDeck();
 
-	cout << "Shuffle " << endl;
-	deck.Shuffle();
-	deck.Shuffle();
+	cout << "Do you want to play blackjack press Y or y for yes or press N or n for no ?" << endl;
+	cin >> answer;
+	while (answer == 'y' || answer == 'Y') {
+		cout << "Shuffle " << endl;
+		deck.Shuffle();
+		deck.Shuffle();
 
-	cout << "Dealing Card" << endl;
-	cout << "               " << endl;
-	for (int i = 0; i < cardDeckNum; i++) {
-		CurrentCard = deck.dealCard();
-		singleCard = CurrentCard.print();
-		gameDeck.push(singleCard);
-	}
-
-
-	for (int i = 0; i < 2; i++) {
-		playerCard = gameDeck.top();
-		playerHand1.push_back(playerCard);
-		gameDeck.pop();
-		playerCard = gameDeck.top();
-		playerHand2.push_back(playerCard);
-		gameDeck.pop();
-		// playerCard = gameDeck.top();
-		// playerHand3.push_back(playerCard);
-		// gameDeck.pop();
-		// playerCard = gameDeck.top();
-		// playerHand4.push_back(playerCard);
-		// gameDeck.pop();
-
-	}
-
-
-
-	cout << "Player's Hand" << endl;
-	for (int i = 0; i < playerHand1.size(); i++) {
-		cout << playerHand1.at(i) << endl;
-
-
-
-	}
-	if (playerHand1.at(0).at(0) == 'A' || playerHand1.at(1).at(0) == 'A') {
-		cout << "Do you want the ace value to be 1 or 11 ?" << endl;
-		cin >> acevalue;
-		playerscore = acevalue;
-		if (playerHand1.at(0).at(0) == 'A') {
-			playerscore += calculateScore(playerHand1.at(1));
-		}
-		else if (playerHand1.at(1).at(0) == 'A') {
-			playerscore += calculateScore(playerHand1.at(0));
-		}
-	}
-	else {
-		for (int i = 0; i < playerHand1.size(); i++) {
-			playerscore += calculateScore(playerHand1.at(i));
+		cout << "Dealing Card" << endl;
+		cout << "               " << endl;
+		for (int i = 0; i < cardDeckNum; i++) {
+			CurrentCard = deck.dealCard();
+			singleCard = CurrentCard.print();
+			gameDeck.push(singleCard);
 		}
 
-	}
 
-	if (playerscore == 21) {
-		cout << "Congrats you received blackjack 21~" << endl;
-	}
-
-
-
-
-
-	//Options for human players 
-
-	cout << "Option 1 Hit" << endl;
-	cout << "Option 2 Stay" << endl;
-	cout << "Option 3 Points" << endl;
-	while (options != 2 && playerscore != 21) {
-		cin >> options;
-
-		if (options == 1) {
+		for (int i = 0; i < 2; i++) {
 			playerCard = gameDeck.top();
 			playerHand1.push_back(playerCard);
 			gameDeck.pop();
-
-			for (int i = 0; i < playerHand1.size(); i++) {
-				cout << playerHand1.at(i) << endl;
-			}
-			if (playerCard.at(0) == 'A') {
-				cout << "Do you want the ace value to be 1 or 11 ?" << endl;
-				cin >> acevalue;
-				playerscore += acevalue;
-			}
-			else {
-				playerscore += calculateScore(playerCard);
-
-			}
-			
-			if (playerscore > 21) {
-				cout << "GG you lose " << endl;
-				playerLost = true;
-				playerTurnend = true;
-				break;
-			}
-			else if (playerscore == 21) {
-				cout << "You Score 21 " << endl;
-				
-				break;
-			}
-
-		}
-
-		else if (options == 2) {
-			playerTurnend = true;
-			break;
-		}
-		else if (options == 3){
-			cout << "Your score is " << playerscore << endl;
-		}
-	}
-
-
-	cout << "Your Hand:" << endl;
-	for (int i = 0; i < playerHand1.size(); i++) {
-		cout << playerHand1.at(i) << endl;
-	}
-	cout << "Your score is:  " << playerscore << endl;
-
-	if(playerTurnend == true){
-		options = 0;
-	}
-
-
-
-	cout << "Player 2's Hand" << endl;
-	cout << "options " << options << endl; //Debugging Purposes
-	for (int i = 0; i < playerHand2.size(); i++) {
-		cout << playerHand2.at(i) << endl;
-
-
-
-	}
-	if (playerHand2.at(0).at(0) == 'A' || playerHand2.at(1).at(0) == 'A') {
-		choice = get_random();
-		if (choice == 1) {
-			cout << "Player 2 has set the Ace Value to 1" << endl;
-			playerscore2 = 1;
-		}
-		else if (choice == 2) {
-			cout << "Player 2 has set the Ace Value to 11" << endl;
-			playerscore2 = 11;
-		}
-
-		if (playerHand2.at(0).at(0) == 'A') {
-			playerscore += calculateScore(playerHand2.at(1));
-		}
-		else if (playerHand2.at(1).at(0) == 'A') {
-			playerscore += calculateScore(playerHand2.at(0));
-		}
-	}
-	else {
-		for (int i = 0; i < playerHand2.size(); i++) {
-			playerscore2 += calculateScore(playerHand2.at(i));
-		}
-		
-
-	}
-
-	cout << "Player 2 score : " << playerscore2 << endl;
-
-	while (options != 2) {
-		if ((playerscore2 >= 2 && playerscore2 <= 13) || (playerscore2 <= playerscore || playerLost == true) ) {
-			options = 1;
-		}
-		else if ( (playerscore >= 14  && playerscore2 <= 20) || (playerscore2 >= playerscore || playerLost == true)) {
-			options = 2;
-		}
-
-		cout << "options" << " " << options << endl;
-
-		if (options == 1) {
 			playerCard = gameDeck.top();
 			playerHand2.push_back(playerCard);
 			gameDeck.pop();
+			// playerCard = gameDeck.top();
+			// playerHand3.push_back(playerCard);
+			// gameDeck.pop();
+			// playerCard = gameDeck.top();
+			// playerHand4.push_back(playerCard);
+			// gameDeck.pop();
 
-			for (int i = 0; i < playerHand2.size(); i++) {
-				cout << playerHand2.at(i) << endl;
+		}
+	
+		cout << "Player's Hand" << endl;
+		for (int i = 0; i < playerHand1.size(); i++) {
+			cout << playerHand1.at(i) << endl;
+
+
+
+		}
+		if (playerHand1.at(0).at(0) == 'A' || playerHand1.at(1).at(0) == 'A') {
+			cout << "Do you want the ace value to be 1 or 11 ?" << endl;
+			cin >> acevalue;
+			playerscore = acevalue;
+			if (playerHand1.at(0).at(0) == 'A') {
+				playerscore += calculateScore(playerHand1.at(1));
 			}
-			if (playerCard.at(0) == 'A') {
-				choice = get_random();
-				if (choice == 1) {
-					cout << "Player 2 has set the Ace Value to 1" << endl;
-					playerscore2 += 1;
+			else if (playerHand1.at(1).at(0) == 'A') {
+				playerscore += calculateScore(playerHand1.at(0));
+			}
+		}
+		else {
+			for (int i = 0; i < playerHand1.size(); i++) {
+				playerscore += calculateScore(playerHand1.at(i));
+			}
+
+		}
+
+		if (playerscore == 21) {
+			cout << "Congrats you received blackjack 21~" << endl;
+		}
+
+
+
+
+
+		//Options for human players 
+
+		cout << "Option 1 Hit" << endl;
+		cout << "Option 2 Stay" << endl;
+		cout << "Option 3 Points" << endl;
+		while (options != 2 && playerscore != 21) {
+			cin >> options;
+
+			if (options == 1) {
+				playerCard = gameDeck.top();
+				playerHand1.push_back(playerCard);
+				gameDeck.pop();
+
+				for (int i = 0; i < playerHand1.size(); i++) {
+					cout << playerHand1.at(i) << endl;
 				}
-				else if (choice == 2) {
-					cout << "Player 2 has set the Ace Value to 11" << endl;
-					playerscore2 += 11;
+				if (playerCard.at(0) == 'A') {
+					cout << "Do you want the ace value to be 1 or 11 ?" << endl;
+					cin >> acevalue;
+					playerscore += acevalue;
 				}
+				else {
+					playerscore += calculateScore(playerCard);
+
+				}
+
+				if (playerscore > 21) {
+					cout << "GG you lose youre score is " << playerscore  << endl;
+					playerLost = true;
+					playerTurnend = true;
+					break;
+				}
+				else if (playerscore == 21) {
+					cout << "You Score 21 " << endl;
+
+					break;
+				}
+
+			}
+
+			else if (options == 2) {
+				playerTurnend = true;
+				break;
+			}
+			else if (options == 3) {
+				cout << "Your score is " << playerscore << endl;
+			}
+		}
+
+
+		// cout << "Your Hand:" << endl;
+		// for (int i = 0; i < playerHand1.size(); i++) {
+		// 	cout << playerHand1.at(i) << endl;
+		// }
+		// cout << "Your score is:  " << playerscore << endl;
+
+		if (playerTurnend == true) {
+			options = 0;
+		}
+
+
+
+		cout << "Player 2's Hand" << endl;
+		cout << "options " << options << endl; //Debugging Purposes
+		for (int i = 0; i < playerHand2.size(); i++) {
+			cout << playerHand2.at(i) << endl;
+
+
+
+		}
+		if (playerHand2.at(0).at(0) == 'A' || playerHand2.at(1).at(0) == 'A') {
+			
+			if (choice == 1) {
+				cout << "Player 2 has set the Ace Value to 1" << endl;
+				playerscore2 = 1;
+			}
+			else if (choice == 2) {
+				cout << "Player 2 has set the Ace Value to 11" << endl;
+				playerscore2 = 11;
+			}
+
+			if (playerHand2.at(0).at(0) == 'A') {
+
+				playerscore2 += calculateScore(playerHand2.at(1));
+			}
+			else if (playerHand2.at(1).at(0) == 'A') {
+				playerscore2 += calculateScore(playerHand2.at(0));
+			}
+		}
+		else {
+			for (int i = 0; i < playerHand2.size(); i++) {
+				playerscore2 += calculateScore(playerHand2.at(i));
+			}
+
+
+		}
+
+		cout << "Player 2 score : " << playerscore2 << endl;
+
+		while (options != 2) {
+			if ((playerscore2 > playerscore)) {
+				dealerHit = false;
+			}
+			else if (playerLost == true) {
+				dealerHit = false;
+			}
+			else if (playerscore2 < playerscore) {
+				dealerHit = true;
+			}
+			else if (playerscore2 == 20) {
+				dealerHit = true;
+			}
+
+			if (dealerHit == true) {
+				options = 1;
+			}
+			else if (dealerHit == false) {
+				options = 2;
+			}
+
+
+			cout << "options" << " " << options << endl;
+
+			if (options == 1) {
+				playerCard = gameDeck.top();
+				playerHand2.push_back(playerCard);
+				gameDeck.pop();
+
+				for (int i = 0; i < playerHand2.size(); i++) {
+					cout << playerHand2.at(i) << endl;
+				}
+				if (playerCard.at(0) == 'A') {
+					if (playerscore2 > 11) {
+						choice = 1;
+					}
+					else if (playerscore2 < 11) {
+						choice = 2;
+					}
+					if (choice == 1) {
+						cout << "Player 2 has set the Ace Value to 1" << endl;
+						playerscore2 += 1;
+					}
+					else if (choice == 2) {
+						cout << "Player 2 has set the Ace Value to 11" << endl;
+						playerscore2 += 11;
+					}
+				}
+				else {
+					playerscore2 += calculateScore(playerCard);
+
+				}
+				cout << "Player 2 score is:  " << playerscore2 << endl;
+				if (playerscore2 > 21) {
+					cout << "GG player 2 lose " << endl;
+					break;
+				}
+				else if (playerscore2 == 21) {
+					cout << "Player 2 received 21 " << endl;
+					break;
+				}
+
+			}
+			else if (options == 2) {
+				break;
+			}
+
+
+
+		}
+
+
+		cout << "Player 2's hand:" << endl;
+		for (int i = 0; i < playerHand2.size(); i++) {
+			cout << playerHand2.at(i) << endl;
+		}
+		cout << "Player 2's score:  " << playerscore2 << endl;
+
+		if (playerscore2 > playerscore) {
+			if (playerscore2 > 21 && playerscore > 21) {
+				cout << "GG you both lose " << endl;
+			}
+			else if (playerscore2 > 21) {
+				cout << "GG you wins " << endl;
 			}
 			else {
-				playerscore2 += calculateScore(playerCard);
-
-			}
-			cout << "Player 2 score is:  " << playerscore2 << endl;
-			if (playerscore2 > 21) {
-				cout << "GG player 2 lose " << endl;
-				break;
-			}
-			else if (playerscore2 == 21) {
-				cout << "Player 2 received 21 " << endl;
-				break;
+				cout << "GG Player 2 Wins " << endl;
 			}
 
 		}
-		else if (options == 2) {
-			break;
+		else if (playerscore2 < playerscore) {
+			if (playerscore2 > 21 && playerscore > 21) {
+				cout << "GG you both lose " << endl;
+			}
+			else if (playerscore > 21) {
+				cout << "GG player 2  wins " << endl;
+			}
+			else {
+				cout << "GG You Win" << endl;
+			}
 		}
-		
 
-     
-	}
+		else if (playerscore2 == playerscore) {
+			cout << "GG both players tied " << endl;
+		}
+		for(int i = 0; i < playerHand1.size(); i++){
+			gameDeck.push(playerHand1.at(i));
+      	}
+      	playerHand1.clear();
+      	for(int i = 0; i < playerHand2.size(); i++){
+      		gameDeck.push(playerHand2.at(i));
+      	}
+      	playerHand2.clear();
+
+      	// if(playerHand1.empty() && playerHand2.empty()){
+      	// 	cout << "Both hands are empty" << endl;
+      	// }
+
+      	// cout << "print deck" << endl;
+      	// deck.printDeck();	
+		cout << "Would you like to play again press Y or y for yes and N or n for no ? " << endl;
+		cin >> answer;
+		if(answer == 'y' || answer == 'Y' ) {
+			playerscore = 0;
+      		playerscore2 = 0;
+      		options = 0;
+      		
+      	
 
 
-	cout << "Player 2's hand:" << endl;
-	for (int i = 0; i < playerHand2.size(); i++) {
-		cout << playerHand2.at(i) << endl;
-	}
-	cout << "Player 2's score:  " << playerscore2 << endl;
+      		continue; //takes the user back to the start of the game where a new random number is set up
 
-	if (playerscore2 > playerscore) {
-		if (playerscore2 > 21 && playerscore > 21) {
-			cout << "GG you both lose " << endl;
-		}
-		else if(playerscore2 > 21) {
-			cout << "GG you wins " << endl;
-		}
-		else {
-			cout << "GG Player 2 Wins " << endl;
-		}
-		
-	}
-	else if (playerscore2 < playerscore) {
-		if (playerscore2 > 21 && playerscore > 21) {
-			cout << "GG you both lose " << endl;
-		}
-		else if(playerscore > 21) {
-			cout << "GG player 2  wins " << endl;
-		}
-		else {
-			cout << "GG You Win" << endl;
-		}
-	}
 
-	else if (playerscore2 == playerscore) {
-		cout << "GG both players tied " << endl;
+   		}    
+  		else if(answer == 'n' || answer == 'N') {
+      		break;  //exits the game when the user decides to quit
+     	}
+
 	}
+	
+
+	
+
+
 
 
 	return 0;
